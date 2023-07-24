c347f85f6bdc217ec45bfac5c42fcd3084ef190e
Pawel <pawel.konop@gmail.com>
2023-07-24 17:14:27 +0200

    feat: enhance tests

---
+++ shared/test/samples.ts
@@ -0,0 +1,150 @@
+import { createCategory, createProduct, createUser } from './helpers';
+
+export const categoryData = {
+  categoryData1: {
+    name: 'testCategoryName1',
+    description: 'testCategoryDescription1',
+  },
+
+  categoryData2: {
+    name: 'testCategoryName2',
+    description: 'testCategoryDescription2',
+  },
+};
+
+// export const category1: any = createCategory(categoryData.categoryData1);
+// export const category2: any = createCategory(categoryData.categoryData2);
+
+export const productData = {
+  productData1: {
+    name: 'testProductName1',
+    description: 'testProductDescription1',
+    unitsOnStock: 2,
+    price: 21.37,
+  },
+
+  productData2: {
+    name: 'testProductName2',
+    description: 'testProductDescription2',
+    unitsOnStock: 23,
+    price: 1,
+  },
+
+  productData3: {
+    name: 'testProductName3',
+    description: 'testProductDescription3',
+    unitsOnStock: 0,
+    price: 999,
+  },
+
+  productData4: {
+    name: 'testProductName4',
+    description: 'testProductDescription4',
+    unitsOnStock: 999,
+    price: 999,
+  },
+};
+
+// const product1: any = createProduct(productData.productData1, category1);
+// const product2: any = createProduct(productData.productData2, category1);
+// const product3: any = createProduct(productData.productData3, category2);
+// const product4: any = createProduct(productData.productData4, category2);
+
+export const userData = {
+  userData1: {
+    username: 'test1@user.com',
+    password: 'tE$Tp4sSw0rd123',
+  },
+
+  userData2: {
+    username: 'test2@user.com',
+    password: 'tE$Tp4sSw0rd123',
+  },
+};
+
+const user1 = createUser(userData.userData1)
+const user2 = createUser(userData.userData2)
+
+// const orderData = {
+//   orderData1: {
+//     productsArray: [
+//       {
+//         productId: product1.id,
+//         quantity: 1,
+//       },
+//       {
+//         productId: product2.id,
+//         quantity: 3,
+//       },
+//     ],
+//   },
+//
+//   orderData2:  {
+//     productsArray: [
+//       {
+//         productId: product1.id,
+//         quantity: 1,
+//       },
+//       {
+//         productId: product2.id,
+//         quantity: 1,
+//       },
+//       {
+//         productId: product3.id,
+//         quantity: 1,
+//       },
+//     ],
+//   },
+//
+//   orderData3: {
+//     productsArray: [
+//       {
+//         productId: product1.id,
+//         quantity: 1,
+//       },
+//     ],
+//   },
+//
+//   orderData4:  {
+//     productsArray: [
+//       {
+//         productId: product4.id,
+//         quantity: 1,
+//       },
+//     ],
+//   }
+// }
+
+// const PartialFilterData = {
+//
+//   partialFilterData1: {
+//     descending: true,
+//     maxPrice: 40,
+//   },
+//
+//   partialFilterData2: {
+//     maxPrice: 800,
+//     minPrice: 500,
+//     categoryIdArr: [category1.id, category2.id]
+//   },
+//
+//   partialFilterData3: {
+//     descending: true,
+//     minPrice: 950,
+//     categoryIdArr: []
+//   },
+//
+//   partialFilterData4: {
+//     ascending: true,
+//     minPrice: 1,
+//     maxPrice: 999,
+//     categoryIdArr: [category1.id, category2.id]
+//   },
+//
+//   partialFilterData5: {
+//     maxPrice: 20,
+//     categoryIdArr: [category2.id]
+//   }
+//
+// }
+
--- src/products/products.service.test.ts
+++ src/products/products.service.test.ts
@@ -1,13 +1,30 @@
-import { clearDataBaseData, createTestingContainer, ITestingContainer } from '../../shared/test/utils';
+import {
+  clearDataBaseData,
+  createTestingContainer,
+  ITestingContainer,
+} from '../../shared/test/utils';
 import { strict as assert } from 'node:assert';
+import { createCategory } from '../../shared/test/helpers';
+import { categoryData, productData } from '../../shared/test/samples';
 
 describe('ProductsService', () => {
+
   let testingContainer: ITestingContainer;
 
-  let categoryData;
+  let category1, category2
+  let product1, product2, product3, product4
 
   before(async () => {
     testingContainer = await createTestingContainer();
+
+    category1 = createCategory(categoryData.categoryData1)
+    category2 = createCategory(categoryData.categoryData2)
+
+    product1 = productData.productData1
+    product1 = productData.productData2
+    product1 = productData.productData3
+    product1 = productData.productData4
+
   });
 
   beforeEach(async () => {
@@ -16,61 +33,57 @@ describe('ProductsService', () => {
 
   it('should create a product with valid data', async () => {
 
-    const categoryData = {
-      name: "testCategoryName",
-      description: "testCategoryDescription"
-    }
 
-    const category = await testingContainer.repositories.categoriesRepository.create(categoryData)
-    await testingContainer.repositories.categoriesRepository.save(category)
+    const result = await testingContainer.services.productsService.create(
 
-    const productData = {
-      name: 'testProductName',
-      description: 'testProductDescription',
-      unitsOnStock: 2,
-      price: 21.37,
-      categoryId: category.id,
-    };
+      {...productData.productData1, categoryId: category1.id}
 
-    const result = await testingContainer.services.productsService.create(productData)
+      );
 
-    assert.strictEqual(result.name, productData.name);
-    assert.strictEqual(result.description, productData.description);
-    assert.strictEqual(result.unitsOnStock, productData.unitsOnStock);
-    assert.strictEqual(result.price, productData.price);
-    assert.strictEqual(result.category, categoryData.name);
-
-  })
+    assert.strictEqual(result.name, product1.name);
+    assert.strictEqual(result.description, product1.description);
+    assert.strictEqual(result.unitsOnStock, product1.unitsOnStock);
+    assert.strictEqual(result.price, product1.price);
+    assert.strictEqual(result.category, product1.name);
+  });
 
   it('should update a product with valid data', async () => {
-
     const categoryData = {
-      name: "testCategoryName",
-      description: "testCategoryDescription"
-    }
+      name: 'testCategoryName',
+      description: 'testCategoryDescription',
+    };
 
-    const category = await testingContainer.repositories.categoriesRepository.create(categoryData)
-    await testingContainer.repositories.categoriesRepository.save(category)
+    const category =
+      await testingContainer.repositories.categoriesRepository.create(
+        categoryData,
+      );
+    await testingContainer.repositories.categoriesRepository.save(category);
 
     const productData = {
       name: 'testProductName',
       description: 'testProductDescription',
       unitsOnStock: 2,
       price: 21.37,
-      category
+      category,
     };
 
-    const product = await testingContainer.repositories.productsRepository.create(productData)
-    await testingContainer.repositories.productsRepository.save(product)
+    const product =
+      await testingContainer.repositories.productsRepository.create(
+        productData,
+      );
+    await testingContainer.repositories.productsRepository.save(product);
 
     const ProductPartialData = {
       name: 'testUpdatedProductName',
       description: 'testUpdatedProductDescription',
       unitsOnStock: 10,
       price: 1.32,
-    }
+    };
 
-    const result = await testingContainer.services.productsService.update(product.id, ProductPartialData)
+    const result = await testingContainer.services.productsService.update(
+      product.id,
+      ProductPartialData,
+    );
 
     assert.strictEqual(result.id, product.id);
     assert.strictEqual(result.name, ProductPartialData.name);
@@ -80,65 +93,82 @@ describe('ProductsService', () => {
     //The price field is using the numeric column type. The numeric type in
     // TypeORM maps the value to a string to preserve full precision and avoid
     // the loss of significant digits.
-    assert.strictEqual(parseFloat(String(result.price)), ProductPartialData.price);
-
-  })
+    assert.strictEqual(
+      parseFloat(String(result.price)),
+      ProductPartialData.price,
+    );
+  });
 
   it('should delete a product with valid data', async () => {
-
     const categoryData = {
-      name: "testCategoryName",
-      description: "testCategoryDescription"
-    }
+      name: 'testCategoryName',
+      description: 'testCategoryDescription',
+    };
 
-    const category = await testingContainer.repositories.categoriesRepository.create(categoryData)
-    await testingContainer.repositories.categoriesRepository.save(category)
+    const category =
+      await testingContainer.repositories.categoriesRepository.create(
+        categoryData,
+      );
+    await testingContainer.repositories.categoriesRepository.save(category);
 
     const productData = {
       name: 'testProductName',
       description: 'testProductDescription',
       unitsOnStock: 2,
       price: 21.37,
-      category
+      category,
     };
 
-    const product = await testingContainer.repositories.productsRepository.create(productData)
-    await testingContainer.repositories.productsRepository.save(product)
+    const product =
+      await testingContainer.repositories.productsRepository.create(
+        productData,
+      );
+    await testingContainer.repositories.productsRepository.save(product);
 
-    await testingContainer.services.productsService.delete(product.id)
+    await testingContainer.services.productsService.delete(product.id);
 
-    const foundProduct = await testingContainer.repositories.productsRepository.findOne({where: {id: product.id}})
-    const foundCategory = await testingContainer.repositories.categoriesRepository.findOne({where: {id: category.id}})
+    const foundProduct =
+      await testingContainer.repositories.productsRepository.findOne({
+        where: { id: product.id },
+      });
+    const foundCategory =
+      await testingContainer.repositories.categoriesRepository.findOne({
+        where: { id: category.id },
+      });
 
-    assert.strictEqual(foundProduct, null)
-    assert.deepStrictEqual(foundCategory, category)
-
-  })
+    assert.strictEqual(foundProduct, null);
+    assert.deepStrictEqual(foundCategory, category);
+  });
 
   it('should filter products with valid partial filter data', async () => {
-
     const categoryData1 = {
-      name: "testCategoryName1",
-      description: "testCategoryDescription1"
-    }
+      name: 'testCategoryName1',
+      description: 'testCategoryDescription1',
+    };
 
     const categoryData2 = {
-      name: "testCategoryName2",
-      description: "testCategoryDescription2"
-    }
+      name: 'testCategoryName2',
+      description: 'testCategoryDescription2',
+    };
 
-    const category1 = await testingContainer.repositories.categoriesRepository.create(categoryData1)
-    await testingContainer.repositories.categoriesRepository.save(category1)
+    const category1 =
+      await testingContainer.repositories.categoriesRepository.create(
+        categoryData1,
+      );
+    await testingContainer.repositories.categoriesRepository.save(category1);
 
-    const category2 = await testingContainer.repositories.categoriesRepository.create(categoryData2)
-    await testingContainer.repositories.categoriesRepository.save(category2)
+    const category2 =
+      await testingContainer.repositories.categoriesRepository.create(
+        categoryData2,
+      );
+    await testingContainer.repositories.categoriesRepository.save(category2);
 
     const productData1 = {
       name: 'testProductName1',
       description: 'testProductDescription1',
       unitsOnStock: 2,
       price: 21.37,
-      category: category1
+      category: category1,
     };
 
     const productData2 = {
@@ -146,7 +176,7 @@ describe('ProductsService', () => {
       description: 'testProductDescription2',
       unitsOnStock: 23,
       price: 1,
-      category: category1
+      category: category1,
     };
 
     const productData3 = {
@@ -154,67 +184,87 @@ describe('ProductsService', () => {
       description: 'testProductDescription3',
       unitsOnStock: 0,
       price: 999,
-      category: category2
+      category: category2,
     };
 
-    const product1 = await testingContainer.repositories.productsRepository.create(productData1)
-    await testingContainer.repositories.productsRepository.save(product1)
+    const product1 =
+      await testingContainer.repositories.productsRepository.create(
+        productData1,
+      );
+    await testingContainer.repositories.productsRepository.save(product1);
 
-    const product2 = await testingContainer.repositories.productsRepository.create(productData2)
-    await testingContainer.repositories.productsRepository.save(product2)
+    const product2 =
+      await testingContainer.repositories.productsRepository.create(
+        productData2,
+      );
+    await testingContainer.repositories.productsRepository.save(product2);
 
-    const product3 = await testingContainer.repositories.productsRepository.create(productData3)
-    await testingContainer.repositories.productsRepository.save(product3)
+    const product3 =
+      await testingContainer.repositories.productsRepository.create(
+        productData3,
+      );
+    await testingContainer.repositories.productsRepository.save(product3);
 
     const partialFilterData1 = {
       descending: true,
       maxPrice: 40,
-    }
+    };
 
     const partialFilterData2 = {
       maxPrice: 800,
       minPrice: 500,
-      categoryIdArr: [category1.id, category2.id]
-    }
+      categoryIdArr: [category1.id, category2.id],
+    };
 
     const partialFilterData3 = {
       descending: true,
       minPrice: 950,
-      categoryIdArr: []
-    }
+      categoryIdArr: [],
+    };
 
     const partialFilterData4 = {
       ascending: true,
       minPrice: 1,
       maxPrice: 999,
-      categoryIdArr: [category1.id, category2.id]
-    }
+      categoryIdArr: [category1.id, category2.id],
+    };
 
     const partialFilterData5 = {
       maxPrice: 20,
-      categoryIdArr: [category2.id]
-    }
+      categoryIdArr: [category2.id],
+    };
 
-    const res1: any = await testingContainer.services.productsService.getFilteredProducts(partialFilterData1)
-    const res2: any = await testingContainer.services.productsService.getFilteredProducts(partialFilterData2)
-    const res3: any = await testingContainer.services.productsService.getFilteredProducts(partialFilterData3)
-    const res4: any = await testingContainer.services.productsService.getFilteredProducts(partialFilterData4)
-    const res5: any = await testingContainer.services.productsService.getFilteredProducts(partialFilterData5)
+    const res1: any =
+      await testingContainer.services.productsService.getFilteredProducts(
+        partialFilterData1,
+      );
+    const res2: any =
+      await testingContainer.services.productsService.getFilteredProducts(
+        partialFilterData2,
+      );
+    const res3: any =
+      await testingContainer.services.productsService.getFilteredProducts(
+        partialFilterData3,
+      );
+    const res4: any =
+      await testingContainer.services.productsService.getFilteredProducts(
+        partialFilterData4,
+      );
+    const res5: any =
+      await testingContainer.services.productsService.getFilteredProducts(
+        partialFilterData5,
+      );
 
-    const final1 = res1.products.map(product => product.id)
-    const final2 = res2.products.map(product => product.id)
-    const final3 = res3.products.map(product => product.id)
-    const final4 = res4.products.map(product => product.id)
-    const final5 = res5.products.map(product => product.id)
+    const final1 = res1.products.map(product => product.id);
+    const final2 = res2.products.map(product => product.id);
+    const final3 = res3.products.map(product => product.id);
+    const final4 = res4.products.map(product => product.id);
+    const final5 = res5.products.map(product => product.id);
 
-    assert.deepStrictEqual(final1, [product1.id, product2.id])
-    assert.deepStrictEqual(final2, [])
-    assert.deepStrictEqual(final3, [product3.id])
-    assert.deepStrictEqual(final4, [product2.id, product1.id, product3.id])
-    assert.deepStrictEqual(final5, [])
-
-  })
-
-
-
+    assert.deepStrictEqual(final1, [product1.id, product2.id]);
+    assert.deepStrictEqual(final2, []);
+    assert.deepStrictEqual(final3, [product3.id]);
+    assert.deepStrictEqual(final4, [product2.id, product1.id, product3.id]);
+    assert.deepStrictEqual(final5, []);
+  });
 });
