673c70aa7d7e38747a35cc9e76973153b0fc68a7
Pawel <pawel.konop@gmail.com>
2023-08-19 20:16:37 +0200

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://localhost:5672'],
+        queue: 'mail-queue',
+        queueOptions: {
+          durable: false,
+        },
+      },
+    },
+  );
+  await app.listenAsync();
+}
+
+bootstrap();
