f641773f559abcf21d2ee14aee2cdd149d819a98
Pawel <pawel.konop@gmail.com>
2023-08-27 23:44:54 +0200

    feat: add swagger annotations to the methods in fake controller

--- src/fake/fake.controller.ts
+++ src/fake/fake.controller.ts
@@ -1,5 +1,11 @@
 import { Controller, Delete, Post } from '@nestjs/common';
 import { FakeService } from './fake.service';
+import {
+  ApiCreatedResponse,
+  ApiInternalServerErrorResponse,
+  ApiNoContentResponse,
+  ApiOperation,
+} from '@nestjs/swagger';
 
 @Controller('fake')
 export class FakeController {
@@ -9,11 +15,21 @@ export class FakeController {
 
 
   @Post()
+
+  @ApiOperation({ summary: 'Generate fake data' })
+  @ApiCreatedResponse({ description: 'Fake data generated successfully.' })
+  @ApiInternalServerErrorResponse({ description: 'Internal server error.' })
+
   generateFakeData(){
     return this.fakeService.generate()
   }
 
   @Delete()
+
+  @ApiOperation({ summary: 'Delete all database data' })
+  @ApiNoContentResponse({ description: 'Fake data deleted successfully.' })
+  @ApiInternalServerErrorResponse({ description: 'Internal server error.' })
+
   deleteFakeData(){
     return this.fakeService.delete()
   }
