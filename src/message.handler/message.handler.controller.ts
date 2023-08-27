e43d83f94f2ba09695e8d439d6a25fc7d5dc516d
Pawel <pawel.konop@gmail.com>
2023-08-27 22:28:19 +0200

    style: update logs for handling message

--- src/message.handler/message.handler.controller.ts
+++ src/message.handler/message.handler.controller.ts
@@ -3,7 +3,8 @@ import { Ctx, EventPattern, RmqContext } from '@nestjs/microservices';
 import { MessageHandlerService } from './message.handler.service';
 import {
   ActivationMessage,
-  PasswordResetMessage, PaymentConfirmationMessage,
+  PasswordResetMessage,
+  PaymentConfirmationMessage,
   WelcomeMessage,
 } from './message';
 
@@ -14,28 +15,36 @@ export class MessageHandlerController {
   @EventPattern('welcome-mail')
   handleWelcomeMail(@Ctx() context: RmqContext): Promise<void> {
     const message: WelcomeMessage = this.getData(context);
-    console.log(`\nReceived message from the queue at ${this.currentTimestamp}, handling welcome mail...`);
+    console.log(
+      `\nReceived message from the queue at ${this.currentTimestamp}, handling welcome mail...`,
+    );
     return this.messageHandlerService.sendWelcomeEmail(message);
   }
 
   @EventPattern('activation-mail')
   handleActivationMail(@Ctx() context: RmqContext) {
     const message: ActivationMessage = this.getData(context);
-    console.log(`\nReceived message from the queue at ${this.currentTimestamp}, handling activation mail...`);
+    console.log(
+      `\nReceived message from the queue at ${this.currentTimestamp}, handling activation mail...`,
+    );
     return this.messageHandlerService.sendActivationEmail(message);
   }
 
   @EventPattern('password-reset-mail')
   handleResetMail(@Ctx() context: RmqContext) {
     const message: PasswordResetMessage = this.getData(context);
-    console.log(`\nReceived message from the queue at ${this.currentTimestamp}, handling password reset mail...`);
+    console.log(
+      `\nReceived message from the queue at ${this.currentTimestamp}, handling password reset mail...`,
+    );
     return this.messageHandlerService.sendPasswordResetEmail(message);
   }
 
   @EventPattern('payment-confirmation-mail')
   handlePaymentConfirmationMail(@Ctx() context: RmqContext) {
     const message: PaymentConfirmationMessage = this.getData(context);
-    console.log(`\nReceived message from the queue at ${this.currentTimestamp}, handling payment confirmation mail...`);
+    console.log(
+      `\nReceived message from the queue at ${this.currentTimestamp}, handling payment confirmation mail...`,
+    );
     return this.messageHandlerService.sendPaymentConfirmationMail(message);
   }
 
@@ -43,7 +52,7 @@ export class MessageHandlerController {
     return JSON.parse(context.getMessage().content.toString());
   }
 
-  get currentTimestamp(){
+  get currentTimestamp() {
     const now = new Date();
     const year = now.getFullYear();
     const month = String(now.getMonth() + 1).padStart(2, '0');
@@ -54,5 +63,4 @@ export class MessageHandlerController {
     const milliseconds = String(now.getMilliseconds()).padStart(3, '0');
     return `${day}-${month}-${year} ${hours}:${minutes}:${seconds}:${milliseconds}`;
   }
-
 }
